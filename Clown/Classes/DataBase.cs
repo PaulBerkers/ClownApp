5fe9aa53f9d1c1642e54c9d4bf1a7ae85b774139
Paul Berkers <paulberkers15@gmail.com>
2020-03-09 19:01:36 +0100

    Add project files.

---
+++ Clown/Classes/DataBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clown.Classes
+{
+    public class DataBase
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; 
+                                                Initial Catalog=Clowns;Integrated Security=True");
+
+        public void AddClown(string FirstName, string LastName, int Age, string Description, byte[] encode)
+        {
+            conn.Open();
+
+            SqlCommand command = conn.CreateCommand();
+            command.CommandText = "INSERT INTO TheClownTable (Name, LastName, Age, Description, ClownFoto) VALUES (@firstname, @lastname, @age, @description, @foto); ";
+
+            SqlParameter blob = new SqlParameter("@foto", SqlDbType.Binary, encode.Length);
+
+            blob.Value = encode;
+            command.Parameters.Add(blob);
+
+            command.Parameters.AddWithValue("@firstname", FirstName);
+            command.Parameters.AddWithValue("@lastname", LastName);
+            command.Parameters.AddWithValue("@age", Age);
+            command.Parameters.AddWithValue("@description", Description);
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+
+    }
+}
---
+++ Clown/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Clown.Classes;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Clown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataBase db = new DataBase();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GetAllClowns();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] encode = getJPGFromImageControl(ClownPhoto.Source as BitmapImage);
+
+            db.AddClown(FirstName.Text, LastName.Text, int.Parse(Age.Text), Description.Text, encode);
+            MessageBox.Show("Je hebt zojuist de volgende clown toegevoegd: " + FirstName.Text + " " + LastName.Text);
+            GetAllClowns();
+        }
+
+        private void GetAllClowns()
+        {
+            try
+            {
+                lvClowns.Children.Clear();
+
+                SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; 
+                                            Initial Catalog=Clowns;Integrated Security=True");
+
+                conn.Open();
+
+                SqlCommand command = conn.CreateCommand();
+                command.CommandText = $"SELECT * FROM TheClownTable;";
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                DataTable dtData = new DataTable();
+                dtData.Load(reader);
+
+                foreach (DataRow row in dtData.Rows)
+                {
+                    StackPanel sp = new StackPanel();
+
+                    Image imPhoto = new Image();
+                    imPhoto.Width = 100;
+                    imPhoto.Height = 100;
+                    imPhoto.HorizontalAlignment = 0;
+
+                    TextBlock tblName = new TextBlock();
+                    tblName.Text += row["Name"].ToString();
+
+                    TextBlock tblLastName = new TextBlock();
+                    tblLastName.Text += row["LastName"].ToString();
+
+                    TextBlock tblAge = new TextBlock();
+                    tblAge.Text += row["Age"].ToString();
+
+                    TextBlock tblDescription = new TextBlock();
+                    tblDescription.Text += row["Description"].ToString();
+
+                    try
+                    {
+                        byte[] data = (byte[])row["ClownFoto"];
+                        using (MemoryStream ms = new MemoryStream(data))
+                        {
+                            var imageSource = new BitmapImage();
+                            imageSource.BeginInit();
+                            imageSource.StreamSource = ms;
+                            imageSource.CacheOption = BitmapCacheOption.OnLoad;
+                            imageSource.EndInit();
+
+                            imPhoto.Source = imageSource;
+                        }
+                    }
+
+                    catch { }
+
+                    sp.Children.Add(imPhoto);
+                    sp.Children.Add(tblName);
+                    sp.Children.Add(tblLastName);
+                    sp.Children.Add(tblAge);
+                    sp.Children.Add(tblDescription);
+
+                    lvClowns.Children.Add(sp);
+
+                }
+
+                conn.Close();
+            }
+            catch (Exception) { }
+        }
+
+        private void SelecteerFoto_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "Selecteer een partijfoto";
+                dialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                                "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                                "Portable Network Graphic (*.png)|*.png";
+
+                if (dialog.ShowDialog() == true)
+                {
+                    ClownPhoto.Source = new BitmapImage(new Uri(dialog.FileName));
+                    ClownPhotoName.Text = dialog.FileName;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Er is een error", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public byte[] getJPGFromImageControl(BitmapImage imageC)
+        {
+            MemoryStream memStream = new MemoryStream();
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(imageC));
+            encoder.Save(memStream);
+            return memStream.ToArray();
+        }
+    }
+}
